df7e2ffe8288c92704a91a7e50d44c77df526efa
Victoridan <victoridan@gmail.com>
2026-05-04 15:37:37 +0500

    графический интерфейс и мейн

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import controller.GameController;
+import model.Cards;
+import model.GameModel;
+import view.BoardPanel;
+import view.MainFrame;
+
+import javax.swing.*;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            List<Cards.Card> cards = Cards.getRandomPairs(8);
+            GameModel model = new GameModel(cards);
+            BoardPanel boardPanel = new BoardPanel();
+            MainFrame frame = new MainFrame(boardPanel);
+            new GameController(model, frame);
+            frame.setVisible(true);
+        });
+    }
+}
---
+++ src/view/MainFrame.java
@@ -0,0 +1,23 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private final BoardPanel boardPanel; // ссылка на игровое поле
+
+    public MainFrame(BoardPanel boardPanel) {
+        this.boardPanel = boardPanel;
+        setTitle("Сопоставление слов");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setLocationRelativeTo(null); // отцентрировать относительно экрана
+
+        boardPanel = new BoardPanel();
+        setLayout(new BorderLayout(10, 10));
+        add(boardPanel, BorderLayout.CENTER);
+    }
+    public BoardPanel getBoardPanel() {
+        return boardPanel;
+    }
+}
